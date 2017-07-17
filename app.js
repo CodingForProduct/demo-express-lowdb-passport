1595c9a259c0aef05b8741d863a18d24883733de
Wai-Yin Kwan <wykhuh@icloud.com>
2017-07-16 21:53:12 -0700

    display all books

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var expressLayouts = require('express-ejs-layouts');
+var low = require('lowdb');
 
 var app = express();
+
+// connect to database
+const db = low('db.json')
 
 // set view engine
 app.set('view engine', 'ejs');
@@ -10,6 +14,13 @@ app.use(expressLayouts);
 // display home page
 app.get('/', function(req, res) {
   res.render('home')
+})
+
+// display all books
+app.get('/books', function(req, res) {
+  var books = db.get('books').value()
+
+  res.render('books', { books: books })
 })
 
 // start server
