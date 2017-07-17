0639be8048b2e03c57631b13520a64f10fe5a45b
Wai-Yin Kwan <wykhuh@icloud.com>
2017-07-17 01:28:09 -0700

    create new book

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var expressLayouts = require('express-ejs-layouts');
 var low = require('lowdb');
 var path = require('path');
+var bodyParser = require('body-parser');
+const uuid = require('uuid');
 
 var app = express();
 
@@ -14,6 +16,10 @@ const db = low(path.join('data', 'db.json'));
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
 
+// bodyParser reads a form's input and stores it in request.body
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(bodyParser.json()); // support json encoded bodies
+
 // display home page
 app.get('/', function(req, res) {
   res.render('home')
@@ -22,8 +28,9 @@ app.get('/', function(req, res) {
 // display all books
 app.get('/books', function(req, res) {
   var books = db.get('books').value()
+  var authors = db.get('authors').value()
 
-  res.render('books', { books: books })
+  res.render('books', { books: books, authors: authors })
 })
 
 // create a new book
