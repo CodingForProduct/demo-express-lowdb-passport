4ff7d7dccc897170a0834e8994e232d4c25309c1
Wai-Yin Kwan <wykhuh@icloud.com>
2017-07-16 21:53:12 -0700

    display all books

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var expressLayouts = require('express-ejs-layouts');
+var low = require('lowdb');
+var path = require('path');
 
 var app = express();
+
+// connect to database
+// path.join will take the parameters and create a path using the
+// right type of slashes (\ vs /) based on the operatin system
+const db = low(path.join('data', 'db.json'));
 
 // set view engine
 app.set('view engine', 'ejs');
@@ -10,6 +17,13 @@ app.use(expressLayouts);
 // display home page
 app.get('/', function(req, res) {
   res.render('home')
+})
+
+// display all books
+app.get('/books', function(req, res) {
+  var books = db.get('books').value()
+
+  res.render('books', { books: books })
 })
 
 // start server
