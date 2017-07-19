cf73ae6a2153600f0e3f7f2945344a2f90518b68
Wai-Yin Kwan <wykhuh@icloud.com>
2017-07-19 13:36:51 -0700

    10. restrict routes based on logged in state

--- routes.js
+++ routes.js
@@ -66,8 +66,8 @@ router.get('/books/:id', function(req, res) {
 var signup_view_path = path.join('auth', 'signup');
 var login_view_path = path.join('auth', 'login');
 
-// display signup page
-router.get('/signup', function(req, res) {
+// display signup page only if user is not logged in
+router.get('/signup', isLoggedOut(), function(req, res) {
   res.render(signup_view_path)
 })
 
@@ -102,9 +102,8 @@ router.post('/signup', function(req, res) {
   authService.signup(options,res);
 })
 
-
-// display login page
-router.get('/login', function(req, res) {
+// display login page  if user is not logged in
+router.get('/login', isLoggedOut(), function(req, res) {
   res.render(login_view_path, { errors: [] })
 })
 
@@ -122,11 +121,47 @@ router.post(
   ),
 )
 
-// display logout
+// logout user
 router.get('/logout', function(req, res) {
   req.logout();
   req.flash('success', 'You are logged out');
   res.redirect('/')
 })
+
+// display profile page if user is logged in
+router.get('/profile', isLoggedIn(), function(req, res) {
+  var dbUser =  db.get('users').find({ id: req.user.id }).value();
+
+  res.render('profile', { dbUser:  dbUser })
+})
+
+//==========================
+// middleware
+//==========================
+
+// isAuthenticated comes from passport;
+// when a user is logged in, isAuthenticated return true.
+
+function isLoggedIn () {
+	return (req, res, next) => {
+    // if there is a logged in user, do the next thing, and execute the
+    // function for the route
+    if (req.isAuthenticated()) { return next() };
+
+    // if there isn't a login user, skip the function for the route, and
+    // redirect to the login page
+    return res.redirect('/login')
+	}
+}
+
+function isLoggedOut () {
+	return (req, res, next) => {
+    // if there isn't a login user, execute the function for the route
+    if (!req.isAuthenticated()) { return next() };
+
+    // if there is a logged in user, redirect
+    return res.redirect('/')
+	}
+}
 
 module.exports = router;
